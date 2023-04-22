730473440244913f384b0c5b36d644e5c12ace2e
moise10r <moiserushanika2006@gmail.com>
2023-04-22 10:53:59 +0200

    Add the movie's component mockup.

---
+++ src/components/Movies/Movie.vue
@@ -0,0 +1,27 @@
+<template>
+  <option :value="movie.url">{{ movie.title }}</option>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'MovieItem',
+  props: {
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {}
+  },
+});
+</script>
+
+<style scoped>
+  .movie-button {
+    padding: 0.8rem;
+    width: 100%;
+    font-size: 1.5rem;
+  }
+</style>
